4b6bc0028bebe7ef77ad11777981f5ba39df7f7a
Timothy Taz <timone427@gmail.com>
2026-03-12 16:09:07 +0100

    FCC Lab - credit Checker

---
+++ js/creditChecker.js
@@ -0,0 +1,30 @@
+let minIncomeForDuplex = 60000;
+let minCreditScoreForDuplex = 700;
+let minIncomeForCondo = 45000;
+let minCreditScoreForCondo = 680;
+let minIncomeForCar = 30000;
+let minCreditScoreForCar = 650; 
+
+function getLoanMessage(annualIncome, creditScore) {
+	if (annualIncome >= minIncomeForDuplex && creditScore >= minCreditScoreForDuplex) {
+		return "You qualify for a duplex, condo, and car loan.";
+	} else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+		return "You qualify for a condo and car loan." ;
+	} else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+		return "You qualify for a car loan.";
+	} else {
+		return "You don't qualify for any loans.";
+	}
+
+	// return annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo ? "You qualify for a condo and car loan.";
+}
+let duplexLoanMsg = getLoanMessage(85000, 850);
+let condoLoanMsg = getLoanMessage(65000, 690);
+let carLoanMsg = getLoanMessage(45000, 660);
+let noLoanMsg = getLoanMessage(25000, 550);
+
+
+console.log(duplexLoanMsg);
+console.log(condoLoanMsg);
+console.log(carLoanMsg);
+console.log(noLoanMsg);
