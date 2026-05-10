dc0406fc82ec16d0378948bdebe8844944a81d52
Timothy Taz <timone427@gmail.com>
2026-05-10 20:43:51 +0100

    new object examples

--- js/objects.js
+++ js/objects.js
@@ -1,15 +1,48 @@
-let agent = { 
-	name: "Taz",
-	age: 21,
-	occupation: "web developer",
-	origin: "Asgard",
-	locaton: "Lagos"
+let player = {
+  name: {
+    fName: "Adam",
+    lName: "Jones",
+  },
+  age: 20,
+  salary: "$360,000",
+  position: "Power Forward",
+  team: "Zuma Suns",
+  contact: {
+    email: "jadams12@yahoo.com",
+    phone: {
+      home: "123-456-7890",
+      work: "098-765-4321",
+    },
+  },
+  addresses: [
+    { type: "home", street: "123 Main St", city: "Anytown" },
+    { type: "work", street: "456 Market St", city: "Workville" },
+  ],
+};
+console.log(player.name.lName);
+console.log(player.addresses[0].city);
+
+
+
+
+
+let agent = {
+  name: "Taz",
+  age: 21,
+  occupation: "web developer",
+  origin: "Asgard",
+  locaton: "Lagos",
 };
 
 // delete agent.age;
-const {origin, locaton, ...remain} = agent; 
+const { origin, locaton, ...remain } = agent;
+console.log(remain);
 
-console.log(remain); 
+console.log(Object.hasOwn(agent, "occupation"));
+console.log(Object.hasOwn(agent, "origin"));
+console.log(Object.hasOwn(agent, "level"));
 
-console.log(agent.name, agent.occupation);
+console.log("name" in agent);
+
+console.log("Name:", agent.name, "Job:", agent.occupation);
 console.log(agent);
