44bec03550b5780dc46d02ab4af5a9f175f41e36
Timothy Taz <timone427@gmail.com>
2026-07-02 12:16:47 +0100

    FFC workshop - sentenceAnalyzer 2nd attempt

--- js/sentenceAnalyzer.js
+++ js/sentenceAnalyzer.js
@@ -1,27 +1,59 @@
 function getVowelCount(sentence) {
-	let vowels = "aeiouAEIOU";
-	let count = 0;
-    for (let char of sentence) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let char of sentence) {
     if (vowels.includes(char)) {
       count++;
     }
   }
-	return count 
+  return count;
 }
 
-let vowel = "Apples are tasty fruits"
+let vowel = "Apples are tasty fruits";
 const vowelCount = getVowelCount(vowel);
 
 console.log(`Vowel Count: ${vowelCount}`);
 
 function getConsonantCount(sentence) {
-	let count = 0
-	let vowels = "aeiouAEIOU";
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+//   const consonant = !vowels;
 
-	for (let char of sentence) {
-		if (!vowels.includes(char)) {
-		count++;
+  for (let char of sentence) {
+    if (/[a-z]/i.test(char)) {
+      if (!vowels.includes(char)) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+let conso = "Coding is fun";
+let conso1 = "Hello World!";
+const consoCount = getConsonantCount(conso1);
+
+console.log(`Consonant Count: ${consoCount}`);
+
+// console.log(getConsonantCount(conso1));
+
+
+function getPunctuationCount(sentence) {
+	let count= 0
+	for (let char of sentence){
+		if (char !== " " && !/[a-z]/i.test(char)) {
+			count ++;
 		}
 	}
-	return count;
+	return count
+}
+console.log(getPunctuationCount("What????!"));
+
+
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+
+console.log(`Punctuation Count: ${punctuationCount}`);
+
+function getWordCount(sentence) {
+	
 }
