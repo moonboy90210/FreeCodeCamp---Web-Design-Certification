df3d6b8fb6cf47d6bae5e38ad83db5d61c4143a8
Timothy Taz <timone427@gmail.com>
2026-07-15 23:56:27 +0100

    spaceRoster lesson 4

--- js/spaceRoster.js
+++ js/spaceRoster.js
@@ -5,8 +5,13 @@ const 	firstAstronaut = { id: 1,
 	isEVAEligible: true,
 	priority: 3,
 }
-const remainingCrew = 
-  { id: 2, name: "Bart", role: "Pilot", isEVAEligible: false, priority: 8 };
+const remainingCrew = [
+  { id: 2, name: "Bart", role: "Pilot", isEVAEligible: false, priority: 8 },
+	{ id: 3, name: "Caroline", role: "Engineer", isEVAEligible: true, priority: 4 },
+  { id: 4, name: "Diego", role: "Scientist", isEVAEligible: false, priority: 1 },
+  { id: 5, name: "Elise", role: "Medic", isEVAEligible: true, priority: 7 },
+  { id: 6, name: "Felix", role: "Navigator", isEVAEligible: true, priority: 6 }
+];
 
 
 function addCrewMember(crew, astronaut) {
@@ -23,6 +28,14 @@ console.log(`Added ${astronaut.name} as ${astronaut.role}`);
 
 addCrewMember(squad, firstAstronaut);
 addCrewMember(squad, remainingCrew); 
+
+
+for (let i = 0; i < remainingCrew.length; i++ ) {
+	addCrewMember(squad, remainingCrew[i]);
+	
+}
+
+
 
 
 
