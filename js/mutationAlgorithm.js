2d252175f3d777433266b5bc77d3fbce1ac47acf
Timothy Taz <timone427@gmail.com>
2026-08-02 11:32:04 +0100

    mutationAlgorithm final!

--- js/mutationAlgorithm.js
+++ js/mutationAlgorithm.js
@@ -1,12 +1,13 @@
-function mutation([strii]) {
- for (let str of strii) {
-	if (str === strii.length) {
-		return true;
-	} else {
+function mutation(strii) {
+
+	const first = strii[0].toLowerCase();
+	const second = strii[1].toLowerCase();
+ for (let char of second) {
+	if (first.indexOf(char) === -1) {
 		return false; 
 	}
-
  }
+ return true; 
 }
 
-console.log(mutation(["hello", "hey"]));
+console.log(mutation(["hello", "helLo"]));
