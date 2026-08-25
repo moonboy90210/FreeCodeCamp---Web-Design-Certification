3bb6b6ebfc9bdf3e1c56757cd4ada7f937a893ab
Timothy Taz <timone427@gmail.com>
2026-08-25 23:54:41 +0100

    missingLetter final!

--- js/missingLetter.js
+++ js/missingLetter.js
@@ -1,4 +1,20 @@
 function fearNotLetter(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    const currentCode = str.charCodeAt(i);
+    const nextCode = str.charCodeAt(i + 1);
+
+    // If the next character isn't exactly +1 in sequence
+    if (nextCode !== currentCode + 1) {
+      return String.fromCharCode(currentCode + 1);
+    }
+  }
+  return undefined; // if no letter is missing
+}   
+
+
+// my first logic attempt 
+
+function fearNotLetter(str) {
 	letters = "abcdefghijklmnopqrstuvwxyz"
 
 	for(let i = 0; i < letters.length; i++){
@@ -8,18 +24,3 @@ function fearNotLetter(str) {
 
 }
 
-
-letters = "abcdefghijklmnopqrstuvwxyz"
-
-	for(let i = 0; i < letters.length; i++){
-
-	  return i = 5; 
-	}
-
-	
-const letters = "abcdefghijklmnopqrstuvwxyz";
-	console.log(letters.length);
-
-	for(let letter of letters){
-	  return letters.length; 
-	}
